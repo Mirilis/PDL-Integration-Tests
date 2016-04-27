9773423737512e84320f54fa922386c2d6aa93ec
Adam M. Hoover <AHoover@grede.foundries>
2016-04-27 14:48:15 -0400

    Initial Commit

---
+++ ProcessCardDataManagerIntegrationTests/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProcessCardDataManagerLibrary;
+
+namespace ProcessCardDataManagerIntegrationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading DataContext");
+            var context = new ProcessDocumentDataContainer();
+            Console.WriteLine("Context Successfully Loaded.");
+
+            var presentation = new ProcessDocumentPresenter();
+            foreach (var document in presentation.DocumentTitles)
+            {
+                Console.WriteLine(document);
+            }
+            foreach (var document in presentation.TemplateTypes)
+            {
+                Console.WriteLine(document);
+            }
+
+            try
+            {
+                presentation.Documents.CreateNewDocument("Cows", "Test");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //foreach (var item in collection)
+            //{
+                
+            //}
+           
+            Console.WriteLine("Press Any Key to Continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
